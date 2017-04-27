b985a8984635de808174f1e2482c508db42c4211
ryon592 <solutionsguyana@gmail.com>
2017-04-26 21:49:54 -0400

    Php Script
    Script For Munchies Mobile App

---
+++ Restaurants.php
@@ -0,0 +1,13 @@
+// Php Script For Munchies to Pull On Android App
+
+<?php
+$connection = mysqli_connect("localhost","root","","Munchies"); 
+$id = $_GET["id"]; 
+$query = "Select * from my_data where id between ($id+1) and ($id+4)";
+$result = mysqli_query($connection,$query); 
+while ($row = mysqli_fetch_assoc($result)) 
+{ $array[] = $row;
+} 
+header('Content-Type:Application/json'); 
+echo json_encode($array); 
+?>
