259b16dc84d3791e584e9f5ed356c020cf87dcb8
CyberSolutionsGy <solutionsguyana@gmail.com>
2017-04-28 15:36:14 -0400

    Add OnClick Listeners on each cardview

--- app/src/main/java/com/solutionsguyana/munchies/CustomAdapter.java
+++ app/src/main/java/com/solutionsguyana/munchies/CustomAdapter.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -21,6 +23,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     private Context context;
     private List<MyData> my_data;
 
+
     public CustomAdapter(Context context, List<MyData> my_data) {
         this.context = context;
         this.my_data = my_data;
@@ -31,14 +34,31 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
 
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card,parent,false);
 
+
+
         return new ViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, final int position) {
 
         holder.description.setText(my_data.get(position).getDescription());
         Glide.with(context).load(my_data.get(position).getImage_link()).into(holder.imageView);
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, " " + my_data.get(position).getDescription(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        holder.imageView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Toast.makeText(context, "You touched " + my_data.get(position).getImage_link(), Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
@@ -51,11 +71,13 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
 
         public TextView description;
         public ImageView imageView;
+        public RelativeLayout cardView;
 
         public ViewHolder(View itemView) {
             super(itemView);
             description = (TextView) itemView.findViewById(R.id.description);
             imageView = (ImageView) itemView.findViewById(R.id.image);
+            cardView = (RelativeLayout) itemView.findViewById(R.id.cardView);
         }
     }
 }
